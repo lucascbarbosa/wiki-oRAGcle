a3dbf7a018aae43092242783a652ac3ba1689727
Lucas Costa Barbosa <lucas.barbosa.0899@poli.ufrj.br>
2025-05-28 16:11:48 -0300

    ♻️ refactor evaluation logic and add metrics for improved performance

--- code/evaluate_rag.py
+++ code/evaluate_rag.py
@@ -3,39 +3,57 @@ import faiss
 import google.generativeai as genai
 import pandas as pd
 from generate_response import generate_response, retrieve_context
+from ragas import evaluate, EvaluationDataset
+from ragas.llms import LangchainLLMWrapper
+from ragas.metrics import (
+    AnswerCorrectness,
+    AnswerRelevancy,
+    AnswerSimilarity,
+    ContextPrecision,
+    ContextRecall,
+    Faithfulness,
+    LLMContextRecall
+)
 from sentence_transformers import SentenceTransformer
 
-
-# Carrega bases e modelos
+# Global variables
 PROCESSED_DATABASE_PATH = "../artifacts/processed_database.parquet"
 FAISS_INDEX_PATH = "../artifacts/faiss_index.index"
 GEMINI_MODEL = "gemini-2.0-flash"
 GEMINI_API_KEY = "AIzaSyAV3VJG9STCErIBXz1LNls0V3SQ_UVi24U"
+
+# Steup api key
 genai.configure(api_key=GEMINI_API_KEY)
 
-# Setup variables
-print("\nSetting up models...")
+print("\nSetting up database, models and metrics...")
 embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 faiss_index = faiss.read_index(FAISS_INDEX_PATH)
 processed_pages_df = pd.read_parquet(PROCESSED_DATABASE_PATH)
 gemini_client = genai.GenerativeModel(model_name=GEMINI_MODEL)
 gemini_chat = gemini_client.start_chat(history=[])
+metrics = [
+    AnswerCorrectness(),
+    AnswerRelevancy(),
+    AnswerSimilarity(),
+    ContextPrecision(),
+    ContextRecall(),
+    Faithfulness(),
+    LLMContextRecall()
+]
+
 
 # Read QA benchmark data
 benchmark_data = pd.read_json("../artifacts/benchmark.json")
 scores = {}
 for subject in benchmark_data.columns:
     subject_qas = benchmark_data[subject]
-    predictions = []
-    references = []
+    dataset = []
     for qa in subject_qas:
         question = qa['question']
         response_ref = qa['answer']
 
-        print(
-            f"Question: {question}\n"
-            f"Reference: {response_ref}\n"
-        )
+        print(f"Question: {question}")
+        print(f"Reference: {response_ref}")
 
         retrieved_context = retrieve_context(
             embedding_model=embedding_model,
@@ -52,28 +70,27 @@ for subject in benchmark_data.columns:
             temperature=0.7
         )
 
-        print(
-            f"Response: {response_pred}\n\n"
+        print(f"Response: {response_pred}\n")
+
+        dataset.append(
+            {
+                'user_input': question,
+                'retrieved_contexts': retrieved_context,
+                'response': response_pred,
+                'reference': response_ref
+            }
         )
 
-        references.append(response_ref)
-        predictions.append(response_pred)
-
-    # Evaluate metrics
-    bleu = evaluate.load("bleu")
-    bleu_score = bleu.compute(
-        predictions=predictions, references=references)['bleu']
-    rouge = evaluate.load("rouge")
-    rouge_score = rouge.compute(
-        predictions=predictions, references=references)
-    rouge_score = (
-        (
-            rouge_score['rouge1'] + rouge_score['rouge2'] +
-            rouge_score['rougeL'] + rouge_score['rougeL']
-        ) / 4
+    evaluation_dataset = EvaluationDataset.from_list(dataset)
+    result = evaluate(
+        dataset=evaluation_dataset,
+        metrics=metrics,
+        llm=LangchainLLMWrapper(gemini_client),
     )
-    scores[subject] = {
-        'bleu': bleu_score,
-        'rouge': float(rouge_score),
-    }
-    print(f'\n{subject}: {scores[subject]}\n')
+    print(result)
+    # # Evaluate metrics
+    # scores[subject] = {
+    #     'bleu': bleu_score,
+    #     'rouge': float(rouge_score),
+    # }
+    # print(f'\n{subject}: {scores[subject]}\n')
