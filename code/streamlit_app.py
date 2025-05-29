2e7b8f99fb77e05c8b8f87706d658a3773a25eb1
Lucas Costa Barbosa <lucas.barbosa.0899@poli.ufrj.br>
2025-05-29 12:23:49 -0300

    🔧 fix paths for processed database and FAISS index; update gemini client initialization

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -7,10 +7,13 @@ from generate_response import generate_response, retrieve_context
 from sentence_transformers import SentenceTransformer
 
 # Carrega bases e modelos
-PROCESSED_DATABASE_PATH = "artifacts/processed_database.parquet"
-FAISS_INDEX_PATH = "artifacts/faiss_index.index"
+PROCESSED_DATABASE_PATH = "../artifacts/processed_database.parquet"
+FAISS_INDEX_PATH = "../artifacts/faiss_index.index"
 GEMINI_MODEL = "gemini-2.0-flash"
 GEMINI_API_KEY = "AIzaSyAV3VJG9STCErIBXz1LNls0V3SQ_UVi24U"
+
+# Steup api key
+genai.configure(api_key=GEMINI_API_KEY)
 
 
 # Setup llm varibles
@@ -20,9 +23,7 @@ if 'llm' not in st.session_state:
     embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
     faiss_index = faiss.read_index(FAISS_INDEX_PATH)
     processed_pages_df = pd.read_parquet(PROCESSED_DATABASE_PATH)
-    gemini_client = genai.Client(
-        api_key=GEMINI_API_KEY
-    )
+    gemini_client = genai.GenerativeModel(model_name=GEMINI_MODEL)
     gemini_chat = gemini_client.start_chat(history=[])
     st.session_state['llm'] = {
         'embedding_model': embedding_model,
@@ -63,14 +64,12 @@ if question := st.chat_input("Enter question"):
         faiss_index=st.session_state['llm']['faiss_index'],
         processed_pages_df=st.session_state['llm']['processed_pages_df'],
         question=question,
-        k=30
+        k=20
     )
     response = generate_response(
-        gemini_chat=gemini_chat,
+        gemini_chat=st.session_state['llm']['gemini_chat'],
         question=question,
         retrieved_context=retrieved_context,
-        max_tokens=512,
-        temperature=0.7
     )
 
     print(f"\nAssistant: {response}")
